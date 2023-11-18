c62f9ed810e4faff7c342139a49219593df68c53
Max Siryk <erotourtes@gmail.com>
2023-11-18 18:03:40 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        'left': '-00px 0px 21px 6px rgba(0,0,0,0.62)',
+      }
+    },
+  },
+  plugins: [],
+}
+
