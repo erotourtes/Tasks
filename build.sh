6eba19c41cd3a033f59b897355d121617ce2aae9
Max Siryk <erotourtes@gmail.com>
2024-01-04 12:19:19 +0200

    update: gh-workflow

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 npm run build
 
-HTML_FILE=$(find . -name index.html | head -n 1)
+HTML_FILE=$(find . -name index.html | tail -n 1)
 
-echo "add base url to Tasks" $HTML_FILE
+echo "add /Tasks/ path" $HTML_FILE
 
-sed -i 's/<head>/<head><base href="\/Tasks\/">/g' $HTML_FILE
+sed -i 's/\/assets\//\/Tasks\/assets\//g' $HTML_FILE
+
+cat $HTML_FILE
